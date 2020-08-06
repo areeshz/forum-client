0889dd2d07f54c4dfed8df774ea02e0ea7994669
areeshz <azindani786@gmail.com>
2020-08-05 21:29:45 -0400

    Removed hard-coded 'Post' data from Feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,54 +6,6 @@ import apiUrl from './../apiConfig.js'
 const Feed = (props) => {
   const [posts, setPosts] = useState(null)
 
-  const bballpost = [
-    {
-      'id': 10,
-      'owner': {
-        'id': 1,
-        'email': 'test1@test1.com'
-      },
-      'title': 'Who is the best player in the NBA today? (besides LeBron James and Kawhi Leonard)',
-      'body': 'A long thing of text which serves as the body of this post. need more and more characters to fill this up and make it seem like theres some words in the body of this post. And again, I do wish to see how this will look if I go even further and make an even bigger text body for this post. Maybe one day this will span more than two lines. Or maybe not, or maybe more.',
-      'created_at': '2020-08-05T20:38:17.454885Z',
-      'updated_at': '2020-08-05T20:38:17.454914Z'
-    },
-    {
-      'id': 11,
-      'owner': {
-        'id': 1,
-        'email': 'test1@test1.com'
-      },
-      'title': 'Who is the best player in the NBA today? (besides LeBron James and Kawhi Leonard)',
-      'body': 'A long thing of text which serves as the body of this post. need more and more characters to fill this up and make it seem like theres some words in the body of this post. And again, I do wish to see how this will look if I go even further and make an even bigger text body for this post. Maybe one day this will span more than two lines. Or maybe not, or maybe more.',
-      'created_at': '2020-08-05T20:38:17.454885Z',
-      'updated_at': '2020-08-05T20:38:17.454914Z'
-    },
-    {
-      'id': 12,
-      'owner': {
-        'id': 1,
-        'email': 'test1@test1.com'
-      },
-      'title': 'Who is the best player in the NBA today? (besides LeBron James and Kawhi Leonard)',
-      'body': 'A long thing of text which serves as the body of this post. need more and more characters to fill this up and make it seem like theres some words in the body of this post. And again, I do wish to see how this will look if I go even further and make an even bigger text body for this post. Maybe one day this will span more than two lines. Or maybe not, or maybe more.',
-      'created_at': '2020-08-05T20:38:17.454885Z',
-      'updated_at': '2020-08-05T20:38:17.454914Z'
-    },
-    {
-      'id': 13,
-      'owner': {
-        'id': 1,
-        'email': 'test1@test1.com'
-      },
-      'title': 'Who is the best player in the NBA today? (besides LeBron James and Kawhi Leonard)',
-      'body': 'A long thing of text which serves as the body of this post. need more and more characters to fill this up and make it seem like theres some words in the body of this post. And again, I do wish to see how this will look if I go even further and make an even bigger text body for this post. Maybe one day this will span more than two lines. Or maybe not, or maybe more.',
-      'created_at': '2020-08-05T20:38:17.454885Z',
-      'updated_at': '2020-08-05T20:38:17.454914Z'
-    }
-  ]
-  console.log(bballpost)
-
   useEffect(() => {
     axios({
       method: 'GET',
