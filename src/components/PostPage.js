9ed395364eb970eff964dae0ffa688f9795eef12
areeshz <azindani786@gmail.com>
2020-08-11 15:53:42 -0400

    Add 'likes' functionality to PostPage component

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from './../apiConfig.js'
+import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Spinner from 'react-bootstrap/Spinner'
@@ -14,6 +15,9 @@ const PostPage = (props) => {
   const postId = props.routeprops.match.params.id
   // State to store post info
   const [post, setPost] = useState(null)
+  const [liked, setLiked] = useState(false)
+  const [numLikes, setNumLikes] = useState(0)
+  const [buttonVariant, setButtonVariant] = useState('outline-primary')
   // State to determine whether component should be in 'showing' or 'editing' mode
   const [version, setVersion] = useState((props.routeprops && props.routeprops.location.state) ? props.routeprops.location.state.version : 'showing')
   // State for handling redirect to feed if the user deletes the post
@@ -29,7 +33,22 @@ const PostPage = (props) => {
     })
       // Set state to have the latest post info
       .then((response) => {
-        setPost(response.data)
+        const retrievedPost = response.data
+        // Update the states with the post data and number of likes
+        setPost(retrievedPost)
+        setNumLikes(retrievedPost.liked_users.length)
+
+        // Determine if the current user has liked the post
+        let isLiked = false
+        if (props.user) {
+          isLiked = retrievedPost.liked_users.some((person) => {
+            return person.id === props.user.id
+          })
+        }
+        // Set the 'liked' state
+        setLiked(isLiked)
+        // Set the like button variant based on the liked state
+        setButtonVariant(isLiked ? 'primary' : 'outline-primary')
       })
       // Failure alert box
       .catch(() => {
@@ -81,6 +100,74 @@ const PostPage = (props) => {
     setVersion('editing')
   }
 
+  // Creates a 'like' connecting the user and post
+  const createLike = () => {
+    axios({
+      method: 'POST',
+      url: apiUrl + '/likes/',
+      headers: {
+        Authorization: `Token ${props.user.token}`
+      },
+      data: {
+        'like': {
+          'post_id': post.id
+        }
+      }
+    })
+      // On success, update the liked state, like counter, and like button style
+      .then(() => {
+        setLiked(true)
+        setNumLikes(numLikes + 1)
+        setButtonVariant('primary')
+      })
+      // Failure alert box
+      .catch(() => {
+        props.msgAlert({
+          heading: 'Like Unsuccessful',
+          message: 'Something went wrong. Please try again later.',
+          variant: 'danger'
+        })
+      })
+  }
+
+  // Removes the 'like' connection between user and post
+  const removeLike = () => {
+    axios({
+      method: 'DELETE',
+      url: apiUrl + '/likes/',
+      headers: {
+        Authorization: `Token ${props.user.token}`
+      },
+      data: {
+        post_id: post.id
+      }
+    })
+      // On success, update the liked state, like counter, and like button style
+      .then((response) => {
+        setLiked(false)
+        setNumLikes(numLikes - 1)
+        setButtonVariant('outline-primary')
+      })
+      // Failure alert box
+      .catch(() => {
+        props.msgAlert({
+          heading: 'Unable to update Like',
+          message: 'Something went wrong. Please try again later.',
+          variant: 'danger'
+        })
+      })
+  }
+
+  const likeHandler = (event) => {
+    event.stopPropagation()
+    // Decides if the post should be liked or unliked
+    if (liked) {
+      removeLike()
+    } else {
+      createLike()
+    }
+  }
+
   const postBoxStyle = {
     border: '1px solid black',
     margin: '25px auto 0',
@@ -126,6 +213,18 @@ const PostPage = (props) => {
     textAlign: 'center'
   }
 
+  const likeBoxStyle = {
+    marginTop: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+
+  const likeButtonStyle = {
+    paddingRight: '13px',
+    marginLeft: '12px'
+  }
+
   // Renders post information  and CommentSection component if in 'showing' mode
   // Renders post edit form if in 'editing' mode
   return (
@@ -147,6 +246,10 @@ const PostPage = (props) => {
           </React.Fragment>}
           <p style={titleStyle}>{post.title}</p>
           <p style={bodyStyle}>{post.body}</p>
+          <div style={likeBoxStyle}>
+            <span>{numLikes} Likes</span>
+            { props.user && <Button variant={buttonVariant} style={likeButtonStyle} size="sm" onClick={likeHandler}>&#x1F44D;</Button>}
+          </div>
         </div>
       }
       {post && (version === 'editing') &&
